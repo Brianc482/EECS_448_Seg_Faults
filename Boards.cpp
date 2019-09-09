abd31c4f66f36dbbde118ee87b881228e297388d
Dalton Yoder <d510y514@ku.edu>
2019-09-09 13:22:44 -0500

    Created Constructor for Boards.h

--- Boards.cpp
+++ Boards.cpp
@@ -1,4 +1,23 @@
 #include "Boards.h"
 
-Boards::Boards(){}
-Boards::~Boards(){}
+Boards::Boards(){
+  /*Default values put into rows and columns.*/
+  rows = 8;
+  cols = 8;
+  /*Creating the board, implemented as a 2-D array of
+    characters on the heap.*/
+  myBoard = new char*[rows];
+  for(int i = 0 ; i < rows ; i++) {
+    myBoard = new char[cols];
+  }
+  /*Fill the default board with "water".*/
+  for(int a = 0 ; a < rows ; a++) {
+    for(int b = 0 ; b < cols ; b++) {
+      myBoard[a][b] = '~';
+    }
+  }
+
+}
+
+Boards::~Boards(){
+}
--- Boards.h
+++ Boards.h
@@ -1,12 +1,25 @@
 #ifndef BOARDS_H
 #define BOARDS_H
 
+#include <iostream>
+#include <string>
+
 class Boards(){
 private:
+	char** myBoard;
+	int rows, cols;
 
 public:
+/****************************************
+		CONSTRUCTOR/DECONSTRUCTOR
+****************************************/
 	Boards();
 	~Boards();
+/****************************************
+		FUNCTIONS
+****************************************/
+
+
 
 };
 #endif
