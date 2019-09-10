f1aa41a22e3f72b105164b2d77c813ebcd9f8f20
Brianc482 <38959797+Brianc482@users.noreply.github.com>
2019-09-10 13:29:04 -0500

    Update Executive.cpp
    Added code to take the column letter and row number from the user. Both are checked for a valid input and the user is prompted to enter values until a valid input is found.

--- Executive.cpp
+++ Executive.cpp
@@ -14,11 +14,34 @@ void Executive::run(){
  newBoard->displayOffensiveBoard();
  newBoard->displayOffensiveBoard();
  newBoard->displayBoth();
- if(newBoard->isValid('P', 4)){
+
+ int row = 0;
+ char column = ' ';
+ std::cout << "\nEnter the column letter: ";
+ std::cin >> column;
+ while(column < 65 || column > 72){
+   if(column < 65 || column > 72){
+   std::cout << "Invalid selection, try again\n";
+   std::cout << "Enter the column letter: ";
+   std::cin >> column;
+  }
+  else{
+    std::cin >> column;
+  }
+ }
+ std::cout << "\nEnter the row number: ";
+ std::cin >> row;
+ while (std::cin.fail() || row > 7 || row < 0){
+   std::cin.clear();
+   std::cin.ignore(INT8_MAX, '\n');
+   std::cout << "Invalid selection, try again.\n";
+   std::cout << "\nEnter the row number: ";
+   std::cin >> row;
+ }
+ if(newBoard->isValid(column, row)){
    std::cout << "Is valid..\n";
  }
  else{
    std::cout << "Invalid location\n";
  }
- delete newBoard;
 }
