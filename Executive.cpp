b14862c26e1b5d4986e5b49e872b18e61fed0755
Brianc482 <38959797+Brianc482@users.noreply.github.com>
2019-09-10 13:03:50 -0500

    Update Executive.cpp

--- Executive.cpp
+++ Executive.cpp
@@ -2,13 +2,23 @@
 #include <fstream>
 #include <stdexcept>
 #include "Executive.h"
+#include "Boards.h"
 
 Executive::Executive(){
-    
-}
-Executive::~Executive(){
 
 }
+Executive::~Executive(){
+}
 void Executive::run(){
- std::cout << "In Executive...\n";
+ Boards* newBoard = new Boards();
+ newBoard->displayOffensiveBoard();
+ newBoard->displayOffensiveBoard();
+ newBoard->displayBoth();
+ if(newBoard->isValid('P', 4)){
+   std::cout << "Is valid..\n";
+ }
+ else{
+   std::cout << "Invalid location\n";
+ }
+ delete newBoard;
 }
