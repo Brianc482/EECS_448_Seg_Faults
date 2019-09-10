ed6e793e1dc4bd6aca15fda61514e7b17bc4f6cf
Dalton Yoder <dyoder97@gmail.com>
2019-09-10 13:10:47 -0500

    Update Boards.cpp

--- Boards.cpp
+++ Boards.cpp
@@ -1,28 +1,4 @@
 #include "Boards.h"
-using namespace std;
-
-Boards::Boards(){
-  /*Default values put into rows and columns.*/
-  rows = 8;
-  cols = 8;
-  /*Creating the board, implemented as a 2-D array of
-    characters on the heap.*/
-  myBoard = new char*[rows];
-  for(int i = 0 ; i < rows ; i++) {
-    myBoard[i] = new char[cols];
-  }
-  /*Fill the default board with "water".*/
-  for(int a = 0 ; a < rows ; a++) {
-    for(int b = 0 ; b < cols ; b++) {
-      myBoard[a][b] = '~';
-    }
-  }
-
-}
-
-Boards::~Boards(){
-  /*Deletes the allocated memory for the board.*/
-  for(int d = 0 ; d < rows ;#include "Boards.h"
 #include <iostream>
 using namespace std;
 
