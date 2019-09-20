a0f743379eefac717216f406f1c83b530dd4d04a
Dalton Yoder <d510y514@ku.edu>
2019-09-20 14:19:30 -0500

    Added Documentation to Executive.h

--- Executive.h
+++ Executive.h
@@ -28,13 +28,41 @@ class Executive{
         *@post returns nothing
         **/
         void run();
+        /**
+         *@pre Game has just been opened.
+         *@post Game starts, or program closes.
+         */
         void displayMenu();
+        /**************************************
+         * Get/Set Functions
+         *************************************/
+        /**
+         *@brief Gets the number of ships desired from the user.
+         */
         void getNumberOfShips();
+        /**
+         *@brief User enters the row to attack.
+         */
         void getRow();
+        /**
+         *@brief User enters the column to attack.
+         */
         void getColumn();
+        /**
+         *@brief User 1 sets their name.
+         */
         void setPlayer1Name();
+        /**
+         *@brief User 2 sets their name.
+         */
         void setPlayer2Name();
+        /**
+         *@brief Returns User 1's name.
+         */
         void getP1Name();
+        /**
+         *@brief Returns User 2's name.
+         */
         void getP2Name();
 };
 #endif
