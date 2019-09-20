cec2938d7c3e341ae79a70523d41470b393067b0
Dalton Yoder <d510y514@ku.edu>
2019-09-20 14:14:53 -0500

    Added Documentation to Players.h

--- Players.h
+++ Players.h
@@ -14,30 +14,88 @@ public:
 	/************************************
 	 * Constructor/Deconstructor
 	 ***********************************/
+	 /**
+	  *@brief creates the player, which has access to their boards.
+		*/
  	Players();
+	/**
+	 *@brief Frees up any assests the player has from memory.
+	 */
 	~Players();
 	/************************************
 	 * Get/Set Functions
 	 ***********************************/
+	 /**
+	  *@brief Grabs both boards to display.
+		*/
 	void getBoards() const;
+	/**
+	 *@brief Displays the board from an offensive standpoint.
+	 */
 	void getOffensiveBoard() const;
+	/**
+	 *@brief Displays the board from a defensive standpoint.
+	 */
 	void getDefensiveBoard() const;
+	/**
+	 *@brief Gets which column the user wants the ship in.
+	 */
 	void getColumn();
+	/**
+	 *@brief Gets which row the user wants the ship in.
+	 */
 	void getRow();
+	/**
+	 *@pre Number of ships being placed known.
+	 *@post All ships are placed on the board.
+	 */
 	void setShips(int);
+	/**
+	 *@pre Name desired for the player is given in exec.
+	 *@post Name is placed in storage for use by Players.
+	 */
 	void setID(std::string);
+	/**
+	 *@brief Returns the player name to the called function.
+	 */
 	std::string getID();
+	/**
+	 *@pre Ships are placed on the board.
+	 *@post Returns true if a ship is at that location.
+	 */
 	bool getHit(char, int);
 
 	/************************************
 	 * Functions
 	 ***********************************/
+	 /**
+	  *@brief If all ships are set, return true.
+		*/
   bool shipsSet();
+	/**
+	 *@brief If the player has no more ships, return true.
+	 */
 	bool hasLost();
+	/**
+	 *@brief Marks my board with hits.
+	 */
 	void markMyHits(char, int);
+	/**
+	 *@brief Marks my board with misses.
+	 */
 	void markMyMisses(char, int);
+	/**
+	 *@brief Marks the opponent's board with hits.
+	 */
 	void markTheirHits(char, int);
+	/**
+	 *@brief Marks the opponent's board with misses.
+	 */
 	void markTheirMisses(char, int);
+	/**
+	 *@pre Column character is given by user.
+	 *@post Character is converted into a numerical value.
+	 */
 	int charConvert(char);
 };
 #endif
