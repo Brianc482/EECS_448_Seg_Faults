e4746e2d37e2d26b045c8b1f335ecae3d0e2db97
Brianc482 <38959797+Brianc482@users.noreply.github.com>
2019-09-20 14:10:43 -0500

    Update Executive.h
    Updated all of the comments needed for Executive.h

--- Executive.h
+++ Executive.h
@@ -23,18 +23,70 @@ class Executive{
         **/
         ~Executive();
         /**
-        *@pre takes a string for the filename
-        *@post run determines which mode to run the program in
-        *@post returns nothing
+        *@pre none
+        *@post none
+        *@post Displays the logo for the game
+        **/
+        void displayLogo();
+        /**
+        *@pre none
+        *@post none
+        *@post Displays the menu for the game
+        **/
+        void displayMenu();
+        /**
+        *@pre none
+        *@post none
+        *@post Handles the majority of functionality within
+        *     the game. ets players name, calls the setShip
+        *     method to begin setting the ships, and handles
+        *     swapping turns for each player until the game is won
         **/
         void run();
-        void displayMenu();
-        void getNumberOfShips();
-        void getRow();
-        void getColumn();
+        /**
+        *@pre none
+        *@post none
+        *@post Prompts the user for a name for player 1
+        **/
         void setPlayer1Name();
+        /**
+        *@pre none
+        *@post none
+        *@post Prompts the user for a name for player 2
+        **/
         void setPlayer2Name();
+        /**
+        *@pre none
+        *@post none
+        *@post Returns the name of player 1 for display purposes
+        **/
         void getP1Name();
+        /**
+        *@pre none
+        *@post none
+        *@post Returns the name of player 2 for display purposes
+        **/
         void getP2Name();
+        /**
+        *@pre none
+        *@post none
+        *@post Prompts the user to enter the number of ships to be
+        *     used during gameplay. Between 1 and 5 ships are allowed
+        **/
+        void getNumberOfShips();
+        /**
+        *@pre none
+        *@post none
+        *@post Repeatedly prmpts the user for a column letter until
+        *     a valid column letter is entered
+        **/
+        void getColumn();
+        /**
+        *@pre none
+        *@post none
+        *@post Repeatedly prmpts the user for a row number until
+        *     a valid row number is entered
+        **/
+        void getRow();
 };
 #endif
